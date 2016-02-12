30cc555522261e24f5e85ae15ce1fe2ff0bc198b
saeedbaeshen <saeed.o.baeshen@gmail.com>
2016-02-11 18:31:11 -0600

    Added files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public int velocidade;
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 15f;
+		velocidade = 30;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (moveSpeed * Input.GetAxis("Horizontal")*Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical")*Time.deltaTime);
+		if(Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(Vector3.up * velocidade * Time.deltaTime);
+		if (Input.GetKey (KeyCode.LeftArrow))
+			transform.Rotate (Vector3.down * velocidade * Time.deltaTime);
+	}
+}
